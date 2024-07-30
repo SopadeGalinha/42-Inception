ec9ab20f2e210412ffe31f7e6f0d9c2401ddc580
SopadeGalinha <jhsocial@icloud.com>
2024-07-30 18:38:56 +0100

    before /home/sopadegalinha fixes

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -5,7 +5,8 @@ while ! wp db check --allow-root --path=/var/www/html/; do
     echo "Waiting for Database to be ready..."
     sleep 1
 done
-wp plugin install redis-cache --activate --allow-root --path=/var/www/html/
+wp plugin install redis-cache \
+     --activate --allow-root --path=/var/www/html/
 wp redis enable --allow-root --path=/var/www/html/
 
 # Start php-fpm
--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -12,8 +12,9 @@ rm -rf latest.tar.gz
 # Change ownership of WordPress files
 chown -R www-data:www-data /var/www/html/
 
-# Download wp-cli directly to the user's bin directory
-wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O /usr/local/bin/wp
+# Download wp-cli to the user's bin directory
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -O \
+	/usr/local/bin/wp
 chmod +x /usr/local/bin/wp
 
 # Configure WordPress
@@ -22,9 +23,6 @@ wp config set DB_NAME $MYSQL_DATABASE --allow-root --path=/var/www/html/
 wp config set DB_USER $MYSQL_USER --allow-root --path=/var/www/html/
 wp config set DB_PASSWORD $MYSQL_PASSWORD --allow-root --path=/var/www/html/
 wp config set DB_HOST mariadb --allow-root --path=/var/www/html/
-
-##################  BONUS  ##################
-################ Redis Cache ################
 
 # Configure Redis Cache
 wp config set WP_REDIS_PORT 6379 --add --type=constant --allow-root --path=/var/www/html/
